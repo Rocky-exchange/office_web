808d9a547708dcb00c4e30f56e868e6e9b9bc493
bek <>
2026-04-18 19:39:05 +0800

    feat: next build

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next';
 
 import { sitemapUrl } from '@/lib/metadata';
 
+export const dynamic = 'force-static';
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next';
 
 import { homepageUrl } from '@/lib/metadata';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
